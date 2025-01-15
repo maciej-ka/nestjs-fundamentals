dd317f16c431bbc52a3c59d3b17e60a13ebe0856
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-01-15 16:43:51 +0100

    feat: add dto to create and update

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,7 +11,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
-import { Coffee } from './entities/coffee.entity';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/create-coffee.dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -48,13 +49,13 @@ export class CoffeesController {
 
   @Post()
   @HttpCode(410)
-  create(@Body() body: Coffee) {
-    this.coffeesService.create(body);
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body: Partial<Coffee>) {
-    this.coffeesService.update(id, body)
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    this.coffeesService.update(id, updateCoffeeDto)
   }
 
   @Delete(':id')
