8659faebc134a0a5fae453f575838ecba3c5f487
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-03-27 12:01:32 +0100

    feat: swagger ui setup

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
 
 async function bootstrap() {
@@ -20,6 +21,15 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor())
+
+  const options = new DocumentBuilder()
+    .setTitle("Iluvcoffee")
+    .setDescription("Coffee application")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
