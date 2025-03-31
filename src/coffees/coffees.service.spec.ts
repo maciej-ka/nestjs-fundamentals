a3739c04cc32606e0d2011a74146bbdd535461d0
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-03-31 14:37:30 +0200

    test: mock dependencies in coffees service

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Flavor } from './entities/flavor.entity';
+import { Coffee } from './entities/coffee.entity';
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getRepositoryToken(Coffee), useValue: {} },
+        { provide: DataSource, useValue: {} },
+        { provide: ConfigService, useValue: { get: () => {}} },
+        { provide: 'COFFEE_BRANDS', useValue: [] },
+        { provide: coffeesConfig.KEY, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
+    // service = await module.resolve(CoffeesService);
+
   });
 
   it('should be defined', () => {
